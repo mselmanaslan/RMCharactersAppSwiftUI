a577ebca51d41e894b662408ee989c4d77e45a51
mselmanaslan <120610572+mselmanaslan@users.noreply.github.com>
2024-03-22 18:55:51 +0300

    Delete Pods directory

--- Pods/Target Support Files/Pods-RMCharactersRev1-RMCharactersRev1UITests/Pods-RMCharactersRev1-RMCharactersRev1UITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RMCharactersRev1_RMCharactersRev1UITests : NSObject
-@end
-@implementation PodsDummy_Pods_RMCharactersRev1_RMCharactersRev1UITests
-@end
